eccca28048bd6f6dd89f7c70535ec0255f19ded6
Pratibha1995 <pratibhacse20@gmail.com>
2025-10-30 11:51:53 +0530

    auth api

---
+++ auth-api-practice/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const User = require('./models/user');
+
+const app = express();
+app.use(express.json());
+
+// Register
+app.post('/register',async (req, res) =>{
+  const {email, password} = req.body;
+
+  const hashedPass = await bcrypt.hash(password, 10);
+  await User.create({ email, password:hashedPass});
+
+  res.json({message: "User register ☑️"});
+});
+
+// Login
+app.post("/login",async (req, res)=>{
+  const {email, password} = req.body;
+
+  const user = await User.findOne({email});
+  if(!user) return res.status(404).json({message:"User not found ❌"});
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch) return res.status(403).json({message:"Wrong password ❌"});
+
+  //creating a access token
+  const token = jwt.sign({id:user._id},process.env.JWT_SECRET, {
+    expiresIn:'1h',
+  });
+
+  res.json({token});
+});
+
+// Protected Route
+app.get('/profile',(req, res) =>{
+  const token = req.headers.authorization?.split(" ")[1];
+  if(!token) return res.status(401).json({message : "No token ❌"});
+
+  jwt.verify(token,process.env.JWT_SECRET,(err, data) =>{
+    if(err) return res.status(401).json({message:"Invalid Token ❌"});
+
+    res.json({message:"Access Granted",data});
+  });
+});
+
+
+const PORT = 5000;
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+  console.log("DB Connected ");
+app.listen(PORT,()=>console.log(`Server running at http://localhost:${PORT}`));
+
+})
+.catch((err)=>console.log(err));
