79f543dcc0f06efe5c8254a083eed5061dd4664c
Ubuntu <ubuntu@ip-172-31-37-239.eu-north-1.compute.internal>
2025-11-25 18:48:24 +0000

    Update cors in server.js

--- auth-api-practice/server.js
+++ auth-api-practice/server.js
@@ -11,12 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://16.171.129.231:5000",
-  ],
+  origin: "http://localhost:5174",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+app.options(/.*/, cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
