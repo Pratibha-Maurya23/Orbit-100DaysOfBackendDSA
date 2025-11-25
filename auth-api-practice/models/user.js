7deb1b5d04807cb20906952df125dcf5cccc5b0d
Pratibha1995 <pratibhacse20@gmail.com>
2025-11-25 18:56:07 +0530

    Update the registration

--- auth-api-practice/models/user.js
+++ auth-api-practice/models/user.js
@@ -4,7 +4,12 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String },
-  age: { type: Number }
+  age: { type: Number },
+   role: {
+    type: String,
+    enum: ["user", "pharmacist", "admin"],
+    default: "user",  
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
