6998e436122898dfdd1f8b042079ca82da7bb0d0
Pratibha1995 <pratibhacse20@gmail.com>
2025-11-25 15:02:34 +0530

    Update the registration

--- auth-api-practice/server.js
+++ auth-api-practice/server.js
@@ -3,11 +3,21 @@ const mongoose = require('mongoose');
 const { authRouter } = require("./routes/authRouter");
 const userRouter = require("./routes/userRouter");
 const {config} = require("./config/config");
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://your-frontend-domain.com"
+  ],
+  credentials: true
+}));
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
