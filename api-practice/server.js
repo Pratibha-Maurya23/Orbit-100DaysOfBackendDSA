26ccd14d6ed5fed32981215f0db3a6de4e902860
PratibhaB.tech23 <pratibhacse20@gmail.com>
2025-11-27 22:05:18 +0530

    Enhance CORS configuration and options handling

--- api-practice/server.js
+++ api-practice/server.js
@@ -10,9 +10,13 @@ const mediRouter = require("./routes/medicineRouter");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5174",
+  origin: ["http://localhost:5174","http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+app.options(/.*/, cors());
 
 app.use(cookieParser());
 app.use(express.json());
@@ -33,3 +37,4 @@ app.listen(PORT,"0.0.0.0",()=>console.log(`Server running at ${PORT}`));
 
 })
 .catch((err)=>console.log(err));
+
