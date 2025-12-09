cf2489a0ffaba9afc6dfc4b29234775134aef29d
PratibhaB.tech23 <pratibhacse20@gmail.com>
2025-12-09 14:53:58 +0530

    Delete api-practice directory

--- api-practice/config/config.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports.config = {
-  port: process.env.PORT || 5000,
-  mongoURL: process.env.MONGO_URL || "mongodb://127.0.0.1:27017/defaultDB",
-  jwtSecret: process.env.JWT_SECRET || "defaultSecret",
-  refreshSecret: process.env.REFRESH_SECRET || "defaultRefreshSecret",
-};
